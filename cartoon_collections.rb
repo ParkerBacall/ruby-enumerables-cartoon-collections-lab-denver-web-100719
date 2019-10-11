c76df3851bf82ba05e7a44969d4abb584cd8fbf6
Parker Bacall <pbacall2@gmail.com>
2019-10-11 02:04:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-    array.include do |word|
+    array.find do |word|
       word == "cheddar" || word == "gouda" || word == "camembert"
     end
 end
