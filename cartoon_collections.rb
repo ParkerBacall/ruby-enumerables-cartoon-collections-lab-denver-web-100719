f8aa007ac23ef70f55e118eb6ca12374dc84e13b
Parker Bacall <pbacall2@gmail.com>
2019-10-11 02:02:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,8 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+    array.include do |word|
+      word = "cheddar" || word = "gouda" || word = "cheddar"
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
